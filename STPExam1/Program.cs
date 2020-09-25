24265c5cade2b7c0afa3c7f4132991db4f3a426f
Michael Sokolov <65177608+stoptrollingmymind@users.noreply.github.com>
2020-09-26 01:12:28 +0300

    Add files via upload

---
+++ STPExam1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoffeMachine coffeMachine = new CoffeMachine();
+            Console.WriteLine(coffeMachine.CreateCoffee(1,3));
+            Console.WriteLine(coffeMachine.CreateCoffee(0, 2));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
