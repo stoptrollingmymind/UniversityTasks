4a6af0366e111ec6a316f00f39f46aa8b40b5e08
Michael Sokolov <65177608+stoptrollingmymind@users.noreply.github.com>
2020-09-26 00:45:04 +0300

    Add files via upload

---
+++ STPExam/Multiplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreparingToExam
+{
+    class Multiplier : IMathOperation
+    {
+
+        public int PerformMathAction(int oper1, int oper2)
+        {
+            return oper1 * oper2 ;
+        }
+    }
+}
---
+++ STPExam/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreparingToExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathFactory mathFactory = new MathFactory();
+            Console.WriteLine(mathFactory.GetMathOperation(0).PerformMathAction(10,6));
+
+
+            Console.ReadKey();
+        }
+    }
+}
