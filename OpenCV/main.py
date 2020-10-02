7af2ef7f36db605f17ac7e2591b8b6cba7255d1b
Michael Sokolov <65177608+stoptrollingmymind@users.noreply.github.com>
2020-10-02 14:00:52 +0300

    Final update

--- OpenCV/main.py
+++ OpenCV/main.py
@@ -1,10 +1,9 @@
 import cv2
                                                             # Mask?
 
-image = cv2.imread("fight_club.jpg", )                     # Downloading image
-cv2.imshow("LOOK AT THIS DUDE", image)
+image = cv2.imread("fight_club.jpg")                     # Downloading image
 b, g, r = cv2.split(image)
-cv2.imshow('GREEEEEN IMAGE DUUUDE', g)                       # Showing green image (a)
+cv2.imshow('Green image', g)                       # Showing green image (a)
 
 clone1 = g.copy()
 clone2 = g.copy()      # Cloning green image (b)
@@ -28,14 +27,12 @@ width2 = clone2.shape[1]
 
 for i in range(0, width2):
     for j in range(0, height2):
-        clone1[j, i] = 0
+        clone2[j, i] = 0
 
-cv2.compare(g, clone1, clone2, cv2.CMP_GE)                  # (e)
+cv2.compare(g, clone1, cv2.CMP_GE, clone2)                  # (e)
 
 difference = cv2.subtract(g, thresh/2, g, clone2)   # Subtracting image and showing results (f)
-cv2.imshow('difference', difference)
-
-cv2.imshow('WHAT THE FUCK IS THIS DUUUDE', clone1)
+cv2.imshow('Difference', difference)
 
 cv2.waitKey(0)
 
