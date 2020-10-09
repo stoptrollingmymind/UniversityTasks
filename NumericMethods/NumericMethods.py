60943d7683689c2d3a449540f7a90c88618bdea6
Michael Sokolov <65177608+stoptrollingmymind@users.noreply.github.com>
2020-10-10 00:16:35 +0300

    Update NumericMethods.py

--- NumericMethods/NumericMethods.py
+++ NumericMethods/NumericMethods.py
@@ -2,119 +2,113 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-first_y = 0
-interval_start = -1
-interval_end = 1
-h = 0.01
-steps = int((interval_end - interval_start)/h + 1)
+real_function = lambda x, y: 1/(x+2*y)
 
 
-def real_function(x, y):
-    return 1 / (x + 2 * y)
-
-
-def add_1_rk4(x, y):
+def k_1_rk4(x, y, h):
     k = real_function(x + h/2, y + h/2 * real_function(x, y))
     return k
 
 
-def add_2_rk4(x, y):
-    k = real_function(x + h/2, y + h/2 * add_1_rk4(x, y))
+def k_2_rk4(x, y, h):
+    k = real_function(x + h/2, y + h/2 * k_1_rk4(x, y, h))
     return k
 
 
-def add_3_rk4(x, y):
-    k = real_function(x + h, y + h * add_2_rk4(x, y))
-    return  k
+def k_3_rk4(x, y, h):
+    k = real_function(x + h, y + h * k_2_rk4(x, y, h))
+    return k
 
 
-def run_kut_4(x, y):
-    y_next = y + h / 6 * (real_function(x, y) + 2 * add_1_rk4(x, y) + 2 * add_2_rk4(x, y) + add_3_rk4(x, y))
-    return y_next
+def run_kut_4(x, h, first_y):
+    y_list = [first_y]
+    for i in range(1, np.size(x)):
+        y = y_list[i-1] + h / 6 * (real_function(x[i-1], y_list[i-1])
+                            + 2 * k_1_rk4(x[i-1], y_list[i-1], h) + 2 * k_2_rk4(x[i-1], y_list[i-1], h)
+                         + k_3_rk4(x[i-1], y_list[i-1], h))
+        y_list.append(y)
+    return y_list
 
 
-def addams_4(x, y, x_1, y_1, x_2, y_2, x_3, y_3):
-    y_next = y + h/24*(55*real_function(x, y)-59*real_function(x_1, y_1) + 37*real_function(x_2, y_2) -
-                   9 * real_function(x_3, y_3))
-    return y_next
+def addams_4(x, h, first_y):
+    y_list = [first_y]
+    y = y_list[0] + h * real_function(x[0], y_list[0])
+    y_list.append(y)
+    y = y_list[1] + h * real_function(x[1], y_list[1])
+    y_list.append(y)
+    y = y_list[2] + h * real_function(x[2], y_list[2])
+    y_list.append(y)
+    for i in range(4, np.size(x)):
+        y = y_list[i-1] + h/24*(55*real_function(x[i-1], y_list[i-1])-59*real_function(x[i-2], y_list[i-2])
+                         + 37*real_function(x[i-3], y_list[i-3]) - 9 * real_function(x[i-4], y_list[i-4]))
+        y_list.append(y)
+    return y_list
 
 
-def addams_3(x, y, x_1, y_1, x_2, y_2):
-    y_next = y + h/12*(23*real_function(x, y) - 16*real_function(x_1, y_1) +5*real_function(x_2, y_2))
-    return y_next
+def addams_3(x, h, first_y):
+    y_list = [first_y]
+    y = y_list[0] + h * real_function(x[0], y_list[0])
+    y_list.append(y)
+    y = y_list[1] + h * real_function(x[1], y_list[1])
+    y_list.append(y)
+    for i in range(3, np.size(x)):
+        y = y_list[i-1] + h/12*(23*real_function(x[i-1], y_list[i-1]) - 16*real_function(x[i-2], y_list[i-2])
+                           + 5*real_function(x[i-3], y_list[i-3]))
+        y_list.append(y)
+    return y_list
 
 
-def eulers_method(x, y):
-    y_next = y + h * real_function(x, y)
-    return y_next
+def euler_method(x, h, first_y):
+    y_list = [first_y]
+    for i in range(1, np.size(x)):
+        y = y_list[i-1] + h * real_function(x[i], y_list[i-1])
+        y_list.append(y)
+    return y_list
 
 
-def improved_eulers_method_1(x, y):
-    y_next = y + h * real_function(x+h/2, y + h/2 * real_function(x, y))
-    return y_next
+def euler_modificated(x, h, first_y):
+    y_list = [first_y]
+    for i in range(1, np.size(x)):
+        y = y_list[i-1] + h * real_function(x[i-1]+h/2, y_list[i-1] + h/2 * real_function(x[i-1], y_list[i-1]))
+        y_list.append(y)
+    return y_list
 
 
-def improved_eulers_method_2(x, y):
-    y_next = y + h / 2 * (real_function(x, y) + real_function(x + h, y + h * real_function(x, y)))
-    return y_next
+def euler_recount(x, h, first_y):
+    y_list = [first_y]
+    for i in range(1, np.size(x)):
+        y = y_list[i-1] + h / 2 * (real_function(x[i-1], y_list[i-1])
+                                 + real_function(x[i-1] + h, y_list[i-1] + h * real_function(x[i-1], y_list[i-1])))
+        y_list.append(y)
+    return y_list
 
 
-def improved_eulers_method_3(x, y, y_previous):
-    y_next = y_previous + 2 * h * real_function(x, y)
-    return y_next
+def euler_central(x, h, first_y):
+    y_list = [first_y]
+    y = y_list[0] + h * real_function(x[0], y_list[0])
+    y_list.append(y)
+    for i in range(2, np.size(x)):
+        y = y_list[i-2] + 2 * h * real_function(x[i-1], y_list[i-1])
+        y_list.append(y)
+    return y_list
 
 
-def list_y_function(foo, list_x, y: float = first_y):
-    list_y = [y]
-
-    if foo == improved_eulers_method_3:
-        y = eulers_method(list_x[0], list_y[0])
-        list_y.append(y)
-        for i in range(2, steps):
-            y = foo(list_x[i - 1], list_y[i - 1], list_y[i - 2])
-            list_y.append(y)
-    elif foo == addams_3:
-        y = eulers_method(list_x[0], list_y[0])
-        list_y.append(y)
-        y = eulers_method(list_x[1], list_y[1])
-        list_y.append(y)
-        for i in range(3, steps):
-            y = foo(list_x[i - 1], list_y[i - 1], list_x[i - 2], list_y[i - 2], list_x[i - 3], list_y[i - 3])
-            list_y.append(y)
-        print(len(list_y))
-    elif foo == addams_4:
-        y = eulers_method(list_x[0], list_y[0])
-        list_y.append(y)
-        y = eulers_method(list_x[1], list_y[1])
-        list_y.append(y)
-        y = eulers_method(list_x[2], list_y[2])
-        list_y.append(y)
-        for i in range(4, steps):
-            y = foo(list_x[i - 1], list_y[i - 1], list_x[i - 2], list_y[i - 2], list_x[i - 2], list_y[i - 3],
-                    list_x[i-4], list_y[i-4])
-            list_y.append(y)
-    else:
-        for i in range(1, steps):
-            y = foo(list_x[i-1], list_y[i-1])
-            list_y.append(y)
-
-    return list_y
-
-
-def render_drawing_num_methods():
-    x = np.linspace(interval_start, interval_end, steps)
-    print(len(x))
-    y = list_y_function(run_kut_4, x)
-    y1 = list_y_function(addams_3, x)
-    y2 = list_y_function(addams_4, x)
-
-    plt.plot(x, y, label='run_kut_4')
-    plt.plot(x, y1, label='addams_3')
-    plt.plot(x, y2, label='addams_4')
-
+def draw_num_method(foo, interval_start, interval_end, str_method: str):
+    h = [0.1, 0.05, 0.01]
+    for i in range(0, np.size(h)):
+        steps = int((interval_end - interval_start) / h[i])
+        x = np.linspace(interval_start, interval_end, steps)
+        y = foo(x, h[i], interval_start)
+        plt.plot(x, y, label= h[i])
+        plt.title(str_method)
     plt.legend()
     plt.show()
 
-
 if __name__ == '__main__':
-    render_drawing_num_methods()
+    draw_num_method(euler_central, -1.0, 1.0, 'euler_central')
+    draw_num_method(euler_recount, -1.0, 1.0, 'euler_recount')
+    draw_num_method(euler_modificated, -1.0, 1.0, 'euler_modificated')
+    draw_num_method(euler_method, -1.0, 1.0, 'euler_method')
+    draw_num_method(addams_3, -1.0, 1.0, 'addams_3')
+    draw_num_method(addams_4, -1.0, 1.0, 'addams_4')
+    draw_num_method(run_kut_4, -1.0, 1.0, 'run_kut_4')
